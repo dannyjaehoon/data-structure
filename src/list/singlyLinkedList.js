69520d6866fef3bfd0f1401266d8f8455efb9781
dannylee9212 <dannylee9212@gmail.com>
2021-03-10 21:02:50 +0900

    feat: implement the singlylinkedlist functionalities: isEmpty, prepend, append, setHead, access, insert, remove and print

--- src/list/singlyLinkedList.js
+++ src/list/singlyLinkedList.js
@@ -8,50 +8,117 @@ class Node {
 class SinglyLinkedList {
   constructor() {
     this.head = null;
+    this.size = 0;
   }
 
-  isEmpty() {}
+  // o(1)
+  isEmpty() {
+    return this.head === null ? true : false;
+  }
+  // o(1)
+  prepend(value) {
+    if (this.isEmpty()) {
+      this.head = new Node(value, null);
+      return;
+    }
+    let current = this.head;
+    let newNode = new Node(value, null);
 
-  prepend(value) {}
+    newNode.next = current;
+    this.head = newNode;
+    this.size += 1;
+  }
 
-  append(value) {}
+  // o(n)
+  append(value) {
+    if (this.isEmpty()) {
+      this.head = new Node(value, null);
+      return;
+    }
 
-  setHead(index) {}
+    let current = this.head;
 
-  access(index) {}
+    while (current.next !== null) {
+      current = current.next;
+    }
+    current.next = new Node(value, null);
+    this.size += 1;
+  }
 
-  insert(index, value) {}
+  // o(n)
+  setHead(index) {
+    if (index > this.size) return;
 
-  remove(index) {}
+    let current = this.head;
+    for (let i = 0; i < index; i++) {
+      current = current.next;
+    }
+    this.head = current;
+    this.size -= index;
+  }
+  // o(n)
+  access(index) {
+    if (index > this.size) return;
 
-  print() {}
-}
+    let current = this.head;
+    for (let i = 0; i < index; i++) {
+      current = current.next;
+    }
+    return current.value;
+  }
+  // o(n)
+  insert(index, value) {
+    if (index === 0) {
+      this.prepend(value);
+      return;
+    }
+    if (index === this.size) {
+      this.append(value);
+      return;
+    }
 
-class Node {
-  constructor(value, next) {
-    this.value = value;
-    this.next = next;
+    let current = this.head;
+    let newNode = new Node(value, null);
+    let prev;
+
+    for (let i = 0; i < index; i++) {
+      prev = current;
+      current = current.next;
+    }
+    prev.next = newNode;
+    newNode.next = current;
+    this.size += 1;
+  }
+  // o(n)
+  remove(index) {
+    if (index === 0) {
+      let current = this.head;
+      this.head = current.next;
+      return;
+    }
+
+    let current = this.head;
+    let prev;
+
+    for (let i = 0; i < index; i++) {
+      prev = current;
+      current = current.next;
+    }
+    prev.next = current.next;
+    this.size -= 1;
+  }
+
+  print() {
+    let result = '';
+    let current = this.head;
+
+    while (current.next !== null) {
+      result += current.value;
+      current = current.next;
+    }
+    result += current.value;
+    return result;
   }
 }
 
-class SinglyLinkedList {
-  constructor() {
-    this.head = null;
-  }
-
-  isEmpty() {}
-
-  prepend(value) {}
-
-  append(value) {}
-
-  setHead(index) {}
-
-  access(index) {}
-
-  insert(index, value) {}
-
-  remove(index) {}
-
-  print() {}
-}
+module.exports = SinglyLinkedList;
--- src/list/singlyLinkedList.test.js
+++ src/list/singlyLinkedList.test.js
@@ -1,4 +1,4 @@
-const SinglyLinkedList = require('./signlyLinkedList');
+const SinglyLinkedList = require('./singlyLinkedList');
 
 let singlyLinkedList;
 beforeEach(() => {
@@ -9,5 +9,180 @@ beforeEach(() => {
 describe('test singlyLinkedList functionalities', () => {
   test('test isEmpty function', () => {
     expect(singlyLinkedList.isEmpty()).toBe(true);
+    singlyLinkedList.append(5);
+    expect(singlyLinkedList.isEmpty()).toBe(false);
+  });
+});
+
+// append
+describe('test singlyLinkedList functionalities', () => {
+  test('test append function', () => {
+    singlyLinkedList.append(5);
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+
+    expect(singlyLinkedList.print()).toBe('51212');
+  });
+});
+
+// prepend
+describe('test singlyLinkedList functionalities', () => {
+  test('test prepend function', () => {
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.prepend(5);
+
+    expect(singlyLinkedList.print()).toBe('51212');
+  });
+});
+
+// setHead
+describe('test singlyLinkedList functionalities', () => {
+  test('test setHead function', () => {
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+    singlyLinkedList.setHead(2);
+
+    expect(singlyLinkedList.print()).toBe('5125');
+  });
+});
+
+describe('test singlyLinkedList functionalities', () => {
+  test('test setHead function', () => {
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+    singlyLinkedList.setHead(0);
+
+    expect(singlyLinkedList.print()).toBe('125125');
+  });
+});
+
+describe('test singlyLinkedList functionalities', () => {
+  test('test setHead function', () => {
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+    singlyLinkedList.setHead(5);
+
+    expect(singlyLinkedList.print()).toBe('5');
+  });
+});
+
+//access
+describe('test singlyLinkedList functionalities', () => {
+  test('test access function', () => {
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+
+    expect(singlyLinkedList.access(2)).toBe(5);
+    expect(singlyLinkedList.access(0)).toBe(1);
+    expect(singlyLinkedList.access(5)).toBe(5);
+  });
+});
+
+//insert
+describe('test singlyLinkedList functionalities', () => {
+  test('test insert function', () => {
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+    singlyLinkedList.insert(2, 9);
+
+    expect(singlyLinkedList.print()).toBe('1295125');
+  });
+});
+
+describe('test singlyLinkedList functionalities', () => {
+  test('test insert function', () => {
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+    singlyLinkedList.insert(0, 9);
+
+    expect(singlyLinkedList.print()).toBe('9125125');
+  });
+});
+
+describe('test singlyLinkedList functionalities', () => {
+  test('test insert function', () => {
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+    singlyLinkedList.insert(5, 9);
+
+    expect(singlyLinkedList.print()).toBe('1251259');
+  });
+});
+
+// remove
+
+describe('test singlyLinkedList functionalities', () => {
+  test('test remove function', () => {
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+    singlyLinkedList.remove(0);
+
+    expect(singlyLinkedList.print()).toBe('25125');
+  });
+});
+
+describe('test singlyLinkedList functionalities', () => {
+  test('test remove function', () => {
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+    singlyLinkedList.remove(3);
+
+    expect(singlyLinkedList.print()).toBe('12525');
+  });
+});
+
+describe('test singlyLinkedList functionalities', () => {
+  test('test remove function', () => {
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(2);
+    singlyLinkedList.append(5);
+    singlyLinkedList.append(1);
+    singlyLinkedList.append(5);
+    singlyLinkedList.append(1);
+
+    singlyLinkedList.remove(5);
+
+    expect(singlyLinkedList.print()).toBe('12515');
   });
 });
